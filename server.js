a482d2f098a2e564278702a1f71406ed5d3730ca
JABUCKLE26 <james.buckley@nology.io>
2021-02-27 18:41:22 +0000

    Removing template server response.

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ app.use(express.json({ extended: false }));
 if (process.env.NODE_ENV === "production") {
   console.log("NODE_ENV set to production");
   app.use(express.static(path.join(__dirname, "/client/build/")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => res.send("API running"));
 }
 
 //Define Routes
