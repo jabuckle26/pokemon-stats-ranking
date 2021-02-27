b99eb1988e59f61488bcd3d2cabc23453823a85e
JABUCKLE26 <james.buckley@nology.io>
2021-02-27 18:34:05 +0000

    Production serving html.

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json({ extended: false }));
 if (process.env.NODE_ENV === "production") {
   console.log("NODE_ENV set to production");
   app.use(express.static(path.join(__dirname, "/client/build/")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("API running"));
 }
-
-app.get("/", (req, res) => res.send("API running"));
 
 //Define Routes
 app.use("/api/pokemon", require("./routes/api/pokemon"));
