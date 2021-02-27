05850a0ff65ca5a85ed40c8d262b5088cb2bcde1
JABUCKLE26 <james.buckley@nology.io>
2021-02-27 18:14:52 +0000

    Fixing express static statement.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
 
-//Init Middleware
-// This line lets us get the data in the routes
 app.use(express.json({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build/")));
+}
 
 app.get("/", (req, res) => res.send("API running"));
 
@@ -15,7 +18,4 @@ app.use("/api/pokemon", require("./routes/api/pokemon"));
 
 const PORT = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "client/build")));
-// }
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
